8ad290b3e271b7a51b0ad8a0fa96e825a06d9297
Khim Bahadur Gurung <khimbahadurgurung@Khims-MacBook-Pro.local>
2022-11-08 17:45:38 +0100

    paragraph added

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,9 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      The Lives of the Most Excellent Painters, Sculptors, and Architects (Italian: Le vite de' più eccellenti pittori, scultori, e architettori), often simply known as The Lives (Italian: Le Vite), is a series of artist biographies written by 16th-century Italian painter and architect Giorgio Vasari, which is considered "perhaps the most famous, and even today the most-read work of the older literature of art",[1] "some of the Italian Renaissance's most influential writing on art",[2] and "the first important book on art history".[3]
+
+Vasari published the work in two editions with substantial differences between them; the first edition, two volumes, in 1550 and the second, three volumes, in 1568 (which is the one usually translated and referred to). One important change was the increased attention paid to Venetian art in the second edition, even though Vasari still was, and has ever since been, criticised for an excessive emphasis on the art of his native Florence.
       </p>
     </div>
   )
